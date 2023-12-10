20bb445315535fdc8e5f0d4a8c32c5aaefe77179
BKRamirezHuertas <ramirezhb3@gmail.com>
2023-12-10 01:33:12 -0500

    ADD_DATABASE_Countries_MODELS

--- server/src/models/Activity.js
+++ server/src/models/Activity.js
@@ -4,9 +4,39 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Activity', {
-    name: {
+    ID: {
+      type: DataTypes.CHAR(3),
+      allowNull: false,
+      primaryKey: true,
+    },
+    nombre: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    imagenDeLaBandera: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isUrl: true,
+    },
+    continente: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subRegion: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    área: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    población: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, { timestamps: false});
 };
--- server/src/models/Country.js
+++ server/src/models/Country.js
@@ -4,9 +4,37 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Country', {
-    name: {
+    ID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    nombre: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    dificultad: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5,
+      },
+    },
+    duracion: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    temporada: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: {
+          args: [['Verano','Otoño','Invierno','Primavera']],
+          msg: 'Debe ser Verano, Otoño, Inviero o Primavera'
+        },
+      },
+    },
+  }, {timestamps: false});
 };
